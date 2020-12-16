421efef7eecc90750fb2736a7774ba06d85a51f8
David Anastasio <23290366+danastasio@users.noreply.github.com>
2020-12-16 09:55:37 -0500

    Enable email verification

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,18 @@ Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
 	Route::resource('admin', AdminPanel::class);
 });
 
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
