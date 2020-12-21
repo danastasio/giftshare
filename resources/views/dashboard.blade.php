a5b25f899eba087197aad41ac450e7fdd6ca5c30
David Anastasio <23290366+danastasio@users.noreply.github.com>
2020-12-21 14:24:50 -0500

    Remove most table elements. Replace with mobile reactive cards. Add collapsible divs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,6 +22,9 @@
 		{{ __('Dashboard') }}
 	</h2>
 </x-slot>
+<div class="pb-4">
+<!-- This is just here to give some space before the first card. Its needed because the first card doesn't get created until the foreach loop. Adding any space there would add space to every card. Adding padding to <h2> would just increase the header, not add space below it. -->
+</div>
 <?php
 $access_users = DB::select('SELECT distinct(users.name) as person_name,users.id
 FROM user_items
@@ -39,7 +42,7 @@ ORDER BY person_name', [auth()->user()->id] );
 	<div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
 		<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
 			<div class="flex">
-				<div class="flex-auto text-2xl mb-4 text-center">No one has shared a list with you</div>
+<div class="flex-auto text-2xl mb-4 text-center">No one has shared a list with you</div>
 			</div>
 			<div class="flex-auto text-lg mb-4 text-center">Have someone add you to their sharing center to view their list here</div>
 		</div>
@@ -48,99 +51,93 @@ ORDER BY person_name', [auth()->user()->id] );
 @endif
 
 @foreach ( $access_users as $person )
-<?php
-$shared_items = DB::select('SELECT items.name, items.description, items.url, items.id, users.name AS person_name, items.id, user_items.claimed, user_items.claimant_id
-FROM user_items
-JOIN items ON items.id = user_items.item_id
-JOIN users ON users.id = user_items.user_id
-WHERE user_items.user_id IN (
-	SELECT owner_id
-	FROM user_users
-	WHERE sharee_id = ?
-	AND owner_id = ?
-)
-ORDER BY person_name', [auth()->user()->id,$person->id] );
-$item_count = 0;
-?>
-<!-- card spacing good. More space before first card though -->
-<!-- display message to user when all items are claimed or there are no items -->
-<div class="py-4">
-	<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-		<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
-<div x-data={show:true}>
-<button  @click="show=!show" type="button">
-			<div class="flex">
-				<div class="flex-auto text-2xl mb-4">{{$person->person_name}}</div>
-			</div>
-</button>
-<div x-show="show">
-			@foreach ($shared_items as $item)
-				@if ($item->claimed == 0 || $item->claimed == 1 && $item->claimant_id == auth()->user()->id)
-					<?php $item_count += 1 ?>
-				@endif
-			@endforeach
-			
-			@if ($item_count == 0)
-				<div class="flex-auto text-lg mb-4 text-center">This person has not added any items to their list or they have all been claimed.</div>
-			@else
-				<table class="w-full text-md rounded mb-4">
-					<thead>
-					<tr class="border-b">
-						<th class="text-left pb-3">Name</th>
-						<th class="text-left pb-3">Item Name</th>
-						<th class="text-left pb-3">Item Details</th>
-						<th class="text-left pb-3">Item Link</th>
-						<th class="text-left pb-3">Claim</th>
-						<th></th>
-					</tr>
-					</thead>
-			@foreach ($shared_items as $item)
-				<?php if (isset(parse_url($item->url)['host'])) {
-					$text = parse_url($item->url)['host'];
-					} else {
-					$text = parse_url($item->url)['path'];
-				}?>
-
-				@if ($item->claimed == 0 || $item->claimed == 1 && $item->claimant_id == auth()->user()->id)
-				<tr class="hover:bg-gray-100">
-					<td> {{$item->person_name}} </td>
-					<td style="word-break: break-word"> {{$item->name}} </td>
-					<td style="word-break: break-word"> {{$item->description}}</td>
-					@if ( !empty($text) )
-						<td> <a href="{{$item->url}}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" target="_blank">{{$text}}</a></td>
+	<?php
+		$shared_items = DB::select('SELECT items.name, items.description, items.url, items.id, users.name AS person_name, items.id, user_items.claimed, user_items.claimant_id
+		FROM user_items
+		JOIN items ON items.id = user_items.item_id
+		JOIN users ON users.id = user_items.user_id
+		WHERE user_items.user_id IN (
+			SELECT owner_id
+			FROM user_users
+			WHERE sharee_id = ?
+			AND owner_id = ?
+		)
+		ORDER BY person_name', [auth()->user()->id,$person->id] );
+		$item_count = 0;
+	?>
+	<div class="py-4">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="bg-gray-100 md:bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
+				<div x-data={show:true}>
+					<button  @click="show=!show" type="button">
+						<div class="flex">
+							<div class="flex-auto text-2xl mb-4">> {{$person->person_name}}</div>
+						</div>
+					</button>
+				<div x-show="show">
+					@foreach ($shared_items as $item)
+						@if ($item->claimed == 0 || $item->claimed == 1 && $item->claimant_id == auth()->user()->id)
+							<?php $item_count += 1 ?>
+						@endif
+					@endforeach
+				
+					@if ($item_count == 0)
+						<div class="flex-auto text-lg mb-4 text-center">
+							This person has not added any items to their list or they have all been claimed.
+						</div>
 					@else
-						<td></td>
+						@foreach ($shared_items as $item)
+							<?php if (isset(parse_url($item->url)['host'])) {
+								$text = parse_url($item->url)['host'];
+								} else {
+								$text = parse_url($item->url)['path'];
+							}?>
+							@if ($item->claimed == 0 || $item->claimed == 1 && $item->claimant_id == auth()->user()->id)
+								<div id="newcard" class="md:flex flex-none hover:bg-gray-300 bg-white overflow-hidden shadow-xl md:shadow-sm rounded-2xl md:rounded-sm p-3 mb-4 md:mb-0">
+									<div class="md:w-1/5 w-full pb-2">
+										<div class="md:hidden"><strong>Person Name</strong></div>
+										{{$person->person_name}}
+									</div>
+									<div class="md:w-1/5 w-full pb-2">
+										<div class="md:hidden"><hr><strong>Item Name</strong></div>
+										{{$item->name}}
+									</div>
+									<div class="md:w-1/5 w-full pb-2">
+										<div class="md:hidden"><hr><strong>Description</strong></div>
+										{{$item->description}}
+									</div>
+									<div class="md:w-1/5 w-full pb-2">
+										@if ( !empty($text))
+										<form target="_blank">
+											<input type="submit" formaction="{{$item->url}}" class="w-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 md:px-4 px-24 rounded-2xl md:rounded-lg" value="{{$text}}">
+										</form>
+										@endif
+									</div>
+									<div class="md:w-1/5 w-full">
+										@if ( $item->claimed == 0 )
+											<form action="{{ route('claim.store') }}" method="post">
+							                                        @csrf
+							                                        <input type="hidden" name="user" id="current_user" value="{{ auth()->user()->id }}">
+												<input type="hidden" name="item" id="item_id" value="{{ $item->id }}">
+							                                        <input type="submit" value="Claim" class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl md:rounded-lg">
+											</form>
+										@elseif ($item->claimed == 1 && $item->claimant_id == auth()->user()->id)
+											<form action="{{ route('claim.destroy', $item->id) }}" method="post">
+							                                        @csrf
+												@method('DELETE')
+							                                        <input type="hidden" name="user" id="current_user" value="{{ auth()->user()->id }}">
+												<input type="hidden" name="item" id="item_id" value="{{ $item->id }}">
+							                                        <input type="submit" value="Un-claim" class="w-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 md:px-4 px-24 rounded-2xl md:rounded-lg">
+											</form>
+										@endif
+									</div>
+								</div>
+							@endif
+						@endforeach
 					@endif
-					<td>
-					@if ( $item->claimed == 0 )
-						<form action="{{ route('claim.store') }}" method="post">
-	                                        @csrf
-	                                        <input type="hidden" name="user" id="current_user" value="{{ auth()->user()->id }}">
-						<input type="hidden" name="item" id="item_id" value="{{ $item->id }}">
-	                                        <input type="submit" value="Claim" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-						</form>
-					@elseif ($item->claimed == 1 && $item->claimant_id == auth()->user()->id)
-						<form action="{{ route('claim.destroy', $item->id) }}" method="post">
-	                                        @csrf
-						@method('DELETE')
-	                                        <input type="hidden" name="user" id="current_user" value="{{ auth()->user()->id }}">
-						<input type="hidden" name="item" id="item_id" value="{{ $item->id }}">
-	                                        <input type="submit" value="Un-claim" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
-						</form>
-						</form>
-					@endif
-					</td>
-				</tr>
-				@endif
-			@endforeach
-		</tbody>
-	</table>
-	@endif
-			
+				</div>	
+			</div>
 		</div>
 	</div>
-</div>
-</div>
-</div>
 @endforeach
 </x-app-layout>
