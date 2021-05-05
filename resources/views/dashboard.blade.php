757c5e82ae753998efc1f936fef319df4b6cf341
David Anastasio <git@davidanastasio.com>
2021-05-05 14:24:55 -0400

    First pass on new landing page. In dire need of reworking

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,6 +22,22 @@
 		{{ __('Dashboard') }}
 	</h2>
 </x-slot>
+<div class="pb-4">
+<!-- This is just here to give some space before the first card. Its needed because the first card doesn't get created until the foreach loop. Adding any space there would add space to every card. Adding padding to <h2> would just increase the header, not add space below it. -->
+</div>
+<?php
+$access_users = DB::select('SELECT distinct(users.name) as person_name,users.id
+FROM user_items
+JOIN items ON items.id = user_items.item_id
+JOIN users ON users.id = user_items.user_id
+WHERE user_items.user_id IN (
+        SELECT owner_id
+        FROM user_users
+        WHERE sharee_id = ?
+)
+ORDER BY person_name', [auth()->user()->id] );
+
+?>
 @if ( empty($access_users) )
 <div class="py-12">
 	<div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
@@ -35,28 +51,14 @@
 </div>
 @endif
 
-@foreach ( $access_users as $person )
-	<?php
-		$shared_items = DB::select('SELECT items.name, items.description, items.url, items.id, users.name AS person_name, items.id, user_items.claimed, user_items.claimant_id
-		FROM user_items
-		JOIN items ON items.id = user_items.item_id
-		JOIN users ON users.id = user_items.user_id
-		WHERE user_items.user_id IN (
-			SELECT owner_id
-			FROM user_users
-			WHERE sharee_id = ?
-			AND owner_id = ?
-		)
-		ORDER BY person_name', [auth()->user()->id,$person['id']] );
-		$item_count = 0;
-	?>
+@foreach ( $shared_items as $person )
 	<div class="py-4">
 		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 			<div class="bg-gray-100 md:bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
 				<div x-data={show:true}>
 					<button  @click="show=!show" type="button">
 						<div class="flex">
-							<div class="flex-auto text-2xl mb-4">> {{ $person['name'] }}</div>
+							<div class="flex-auto text-2xl mb-4">> {{ $person[0]->name }}</div>
 						</div>
 					</button>
 
@@ -81,28 +83,12 @@
 					<div class="invisible md:visible">
 						<hr>
 					</div>
-					@foreach ($shared_items as $item)
-						@if ($item->claimed == 0 || $item->claimed == 1 && $item->claimant_id == auth()->user()->id)
-							<?php $item_count += 1 ?>
-						@endif
-					@endforeach
-
-					@if ($item_count == 0)
-						<div class="flex-auto text-lg mb-4 text-center">
-							This person has not added any items to their list or they have all been claimed.
-						</div>
-					@else
-						@foreach ($shared_items as $item)
-							<?php if (isset(parse_url($item->url)['host'])) {
-								$text = parse_url($item->url)['host'];
-								} else {
-								$text = parse_url($item->url)['path'];
-							}?>
-							@if ($item->claimed == 0 || $item->claimed == 1 && $item->claimant_id == auth()->user()->id)
+						@foreach($person as $items)
+							@foreach($items['items'] as $item)
 								<div id="newcard" class="md:flex flex-none hover:bg-gray-300 bg-white overflow-hidden shadow-xl md:shadow-sm rounded-2xl md:rounded-sm p-3 mb-4 md:mb-0 border md:border-transparent">
 									<div class="md:w-1/5 w-full pb-2">
 										<div class="md:hidden"><strong>Person Name</strong></div>
-										{{ $person['name'] }}
+										{{$person[0]->name}}
 									</div>
 									<div class="md:w-1/5 w-full pb-2">
 										<div class="md:hidden"><hr><strong>Item Name</strong></div>
@@ -113,11 +99,9 @@
 										{{$item->description}}
 									</div>
 									<div class="md:w-1/5 w-full pb-2">
-										@if ( !empty($text))
 										<form target="_blank">
-											<input type="submit" formaction="{{$item->url}}" class="w-full md:w-auto bg-green-500 hover:bg-green-700 text-white font-bold py-2 md:px-4 px-24 rounded-2xl md:rounded-lg" value="{{$text}}">
+											<input type="submit" formaction="{{$item->url}}" class="w-full md:w-auto bg-green-500 hover:bg-green-700 text-white font-bold py-2 md:px-4 px-24 rounded-2xl md:rounded-lg" value="{{$item->url}}">
 										</form>
-										@endif
 									</div>
 									<div class="md:w-1/5 w-full">
 										@if ( $item->claimed == 0 )
@@ -138,9 +122,8 @@
 										@endif
 									</div>
 								</div>
-							@endif
 						@endforeach
-					@endif
+						@endforeach
 				</div>
 			</div>
 		</div>
