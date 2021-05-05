0093bf7af21861fc655fe65ebb137c4dfc6db2e3
David Anastasio <git@davidanastasio.com>
2021-05-05 10:32:16 -0400

    Add livewire components for claim button

---
+++ app/Http/Livewire/ClaimItem.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ClaimItem extends Component
+{
+    public function render()
+    {
+        return view('livewire.claim-item');
+    }
+}
