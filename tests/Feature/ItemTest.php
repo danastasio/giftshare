c74b97964ad64711db3bf6b11a2cf5a0b5b38497
David Anastasio <git@davidanastasio.com>
2021-07-20 12:09:16 -0400

    Feature testing for Items and Shares (incomplete)

--- tests/Feature/ItemTest.php
+++ tests/Feature/ItemTest.php
@@ -12,28 +12,83 @@ use Tests\TestCase;
 class ItemTests extends TestCase
 {
 	use DatabaseTransactions;
-    /**
-     * Test to make sure you can create an item.
-     *
-     * @return void
-     */
+	/**
+	 * Test to make sure you can create an item.
+	 *
+	 * @return void
+	 */
 	public function testCreateItemWithAllAttributes(): void
 	{
 		$user = User::factory()->create();
-		$item = [
+		$payload = array(
 			'name' => 'item name',
 			'description' => 'this is a test',
 			'url' => 'https://google.com',
-		];
+		);
 
-		$response = $this->be($user)->post(route('item.store'), $item);
-		$id = Item::where('name', $item['name'])->where('description', $item['description'])->value('id');
-		$dbitem = Item::find($id);
-		$this->assertNotNull($dbitem);
+		$response = $this->be($user)->post(route('item.store'), $payload);
+		$id = Item::where('name', $payload['name'])
+			->where('description', $payload['description'])
+			->where('url', $payload['url'])
+			->value('id');
+		$item = Item::find($id);
+		$this->assertNotNull($item);
 		$response->assertSessionHas(['success' => "Item added"]);
-		$this->assertEquals($item['name'], $dbitem->name);
-		$this->assertEquals($item['description'], $dbitem->description);
-		$this->assertEquals($item['url'], $dbitem->url);
+		$this->assertEquals($payload['name'], $item->name);
+		$this->assertEquals($payload['description'], $item->description);
+		$this->assertEquals($payload['url'], $item->url);
+		$response->assertRedirect();
+	}
+
+	/**
+	 * Test to make sure you can create an item without a description
+	 *
+	 * @returns void
+	 */
+	public function testCreateItemWithoutDescription()
+	{
+		$user = User::factory()->create();
+		$payload = array(
+			'name' => 'Fake Item',
+			'url'  => 'https://xkcd.com',
+		);
+
+		$response = $this->be($user)->post(route('item.store'), $payload);
+		$id = Item::where('name' , $payload['name'])
+			->where('url', $payload['url'])
+			->value('id');
+		$item = Item::find($id);
+		$this->assertNotNull($item);
+			$response->assertSessionHas(['success' => "Item added"]);
+		$this->assertEquals($payload['name'], $item->name);
+		$this->assertEquals(null, $item->description);
+		$this->assertEquals($payload['url'], $item->url);
+		$response->assertRedirect();
+	}
+
+	/**
+	 * Test to make sure you can create an item without a url
+	 *
+	 * @returns void
+	 */
+	public function testCreateItemWithoutUrl()
+	{
+		$user = User::factory()->create();
+		$payload = array(
+			'name' 		=> 'Fake Item',
+			'description' => 'The item description goes here',
+		);
+
+		$response = $this->be($user)->post(route('item.store'), $payload);
+		$id = Item::where('name' , $payload['name'])
+			->where('description', $payload['description'])
+			->value('id');
+		$item = Item::find($id);
+		$this->assertNotNull($item);
+			$response->assertSessionHas(['success' => "Item added"]);
+		$this->assertEquals($payload['name'], $item->name);
+		$this->assertEquals(null, $item->url);
+		$this->assertEquals($payload['description'], $item->description);
 		$response->assertRedirect();
 	}
 }
