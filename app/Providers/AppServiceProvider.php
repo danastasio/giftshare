700ccbd206f074ab19592884904825c1d1d26703
David Anastasio <github@davidanastasio.com>
2021-09-15 14:48:42 -0400

    Initial re-commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    	if (env('APP_ENV') == 'production') {
+    		URL::forceScheme('https');
+    	}
+    }
+}
