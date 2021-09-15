42a81c150ad3e198b0c829f06ca633a74fc3bbe8
David Anastasio <github@davidanastasio.com>
2021-09-15 15:24:23 -0400

    Add handling for if user tries to make a change for another user

--- app/Policies/SharePolicy.php
+++ app/Policies/SharePolicy.php
@@ -10,95 +10,103 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 
 class SharePolicy
 {
-    use HandlesAuthorization;
+	use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can view any models.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @return mixed
+	 */
+	public function viewAny(User $user)
+	{
+		//
+	}
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\UserUsers  $userUsers
-     * @return mixed
-     */
-    public function view(User $user, UserUsers $userUsers)
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can view the model.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @param  \App\Models\UserUsers  $userUsers
+	 * @return mixed
+	 */
+	public function view(User $user, UserUsers $userUsers)
+	{
+		//
+	}
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        if (Auth::check()) {
-            return Response::allow();
-        } else {
-            return Response::deny();
-        }
-    }
+	/**
+	 * Determine whether the user can create models.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @return mixed
+	 */
+	public function create(User $user = null)
+	{
+		if ($user === null) {
+			return Response::deny();
+		}
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\UserUsers  $userUsers
-     * @return mixed
-     */
-    public function update(User $user, UserUsers $userUsers)
-    {
-        //
-    }
+		if (Auth::check()) {
+			return Response::allow();
+		} else {
+			return Response::deny();
+		}
+	}
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\UserUsers  $userUsers
-     * @return mixed
-     */
-    public function delete(User $user, UserUsers $share)
-    {
-        if (Auth::check() && (int)$share->owner_id === (int)$user->id) {
-            return Response::allow();
-        } else {
-            return Response::deny();
-        }
-    }
+	/**
+	 * Determine whether the user can update the model.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @param  \App\Models\UserUsers  $userUsers
+	 * @return mixed
+	 */
+	public function update(User $user, UserUsers $userUsers)
+	{
+		//
+	}
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\UserUsers  $userUsers
-     * @return mixed
-     */
-    public function restore(User $user, UserUsers $userUsers)
-    {
-        //
-    }
+	/**
+	 * Determine whether the user can delete the model.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @param  \App\Models\UserUsers  $userUsers
+	 * @return mixed
+	 */
+	public function delete(User $user = null, UserUsers $share = null)
+	{
+		if ($share === null || $user === null) {
+			return Response::deny();
+		}
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\UserUsers  $userUsers
-     * @return mixed
-     */
-    public function forceDelete(User $user, UserUsers $userUsers)
-    {
-        //
-    }
+		if (Auth::check() && (int)$share->owner_id === (int)$user->id) {
+			return Response::allow();
+		} else {
+			return Response::deny();
+		}
+	}
+
+	/**
+	 * Determine whether the user can restore the model.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @param  \App\Models\UserUsers  $userUsers
+	 * @return mixed
+	 */
+	public function restore(User $user, UserUsers $userUsers)
+	{
+		//
+	}
+
+	/**
+	 * Determine whether the user can permanently delete the model.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @param  \App\Models\UserUsers  $userUsers
+	 * @return mixed
+	 */
+	public function forceDelete(User $user, UserUsers $userUsers)
+	{
+		//
+	}
 }
