8273561cf2132007e4a01647ed283d2b074e25a5
David Dominic Anastasio <danastasio@davidanastasio.com>
2021-10-04 20:40:25 -0400

    Updates from prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-    	if (env('APP_ENV') == 'production') {
-    		URL::forceScheme('https');
-    	}
+    	URL::forceScheme('https');
     }
 }
