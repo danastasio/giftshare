81259c5ced08e595413dea45d2a3878f65c44aa4
David Anastasio <dev@davidanastasio.com>
2021-10-04 20:55:54 -0400

    Finally end my misery with this file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-    	URL::forceScheme('https');
+	if (app()->env === "production") {
+		URL::forceScheme('https');
+	}
     }
 }
