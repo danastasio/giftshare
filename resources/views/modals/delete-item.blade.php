4097580d250558221b167aa552167f53dfa19745
David Anastasio <github@davidanastasio.com>
2021-11-12 12:56:27 -0500

    Update to look better in mobile

--- resources/views/modals/delete-item.blade.php
+++ resources/views/modals/delete-item.blade.php
@@ -1,6 +1,6 @@
 <div id="{{ $name }}" class="invisible fixed top-0 left-0">
 	<div class="bg-gray-200 dark:bg-gray-500 dark:bg-opacity-70 bg-opacity-80 grid place-items-center h-screen w-screen">
-		<div class="p-5 w-1/4 bg-white dark:bg-gray-600 rounded-lg grid grid-cols-1 gap-3 shadow-xl">
+		<div class="p-5 w-3/4 md:w-1/4 bg-white dark:bg-gray-600 rounded-lg grid grid-cols-1 gap-3 shadow-xl">
 		<form method="post" action="{{ route($route, $id) }}">
 			@csrf
 			@method("DELETE")
