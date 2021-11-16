fbcfd347e1a2098167cd015f7e42e5973c56482d
David Anastasio <dev@davidanastasio.com>
2021-11-15 21:53:08 -0500

    Update dbupdate script
    Add three new columns related to the redesign.

--- dbupdate.sh
+++ dbupdate.sh
@@ -1,2 +1,3 @@
 echo "alter table items add column deleted_at DATETIME;" | sqlite3 database/database.db
+echo "alter table items add column purchased BOOLEAN;" | sqlite3 database/database.db
 echo "alter table user_users add column deleted_at DATETIME;" | sqlite3 database/database.db
