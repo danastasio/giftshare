7cb45661ca89b6a4c7b248da4f9c13929ed060ba
David Anastasio <github@davidanastasio.com>
2021-11-17 10:28:40 -0500

    Change wording to work better in mobile view

--- resources/views/livewire/toggle-purchase.blade.php
+++ resources/views/livewire/toggle-purchase.blade.php
@@ -2,6 +2,6 @@
 	@if ($this->purchased)
 		<button wire:click="toggle" class="bg-yellow-300 text-center my-auto p-2 w-full rounded-lg text-black font-semibold">Unpurchase</button>
 	@else
-		<button wire:click="toggle" class="bg-blue-300 text-center my-auto p-2 w-full rounded-lg text-black font-semibold">Mark Purchased</button>
+		<button wire:click="toggle" class="bg-blue-300 text-center my-auto p-2 w-full rounded-lg text-black font-semibold">Mark Bought</button>
 	@endif
 </div>
