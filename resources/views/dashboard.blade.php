6f42375c8fee395473998139a44aaf47b46d5a2b
David Anastasio <github@davidanastasio.com>
2021-11-24 11:39:25 -0500

    Initial support for privacy mode on the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -38,28 +38,37 @@
 		<div class="py-4">
 			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 				<div class="mx-2 rounded-md bg-gray-100 md:bg-white dark:bg-gray-600 overflow-hidden shadow-xl sm:rounded-lg p-5">
-					<button type="button" class="flex mb-4" onclick="toggleSection('{{ $share->id }}')" id="name{{ $share->id}}">
-						<div class="mr-4 my-auto">
-							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-200 my-auto transform rotate-90" fill="none" viewBox="0 0 24 24" stroke="currentColor" id="chevron{{ $share->id }}">
-								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-							</svg>
-						</div>
-						<div>
-							@if ($share->owner->profile_photo_path)
-								<img alt="profile picture" src="{{ url('/storage/' . $share->owner->profile_photo_path) }}" class="w-8 rounded-full mr-3">
-							@else
-								<img alt="generated profile picture" src="{{ url('https://ui-avatars.com/api/?name=' . $share->owner->name . '&background=random&length=1&size=128') }}" class="w-8 rounded-full mr-3">
-							@endif
-						</div>
-						<div class="text-2xl dark:text-gray-200">
-							{{ $share->owner->name }}
+				<div class="flex">
+				<div class="flex-none">
+					<button type="button" class="my-auto" onclick="toggleSection('{{ $share->id }}')" id="name{{ $share->id}}">
+						<div class="flex">
+							<div class="mr-4 my-auto">
+								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-200 my-auto transform rotate-90" fill="none" viewBox="0 0 24 24" stroke="currentColor" id="chevron{{ $share->id }}">
+									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+								</svg>
+							</div>
+							<div>
+								@if ($share->owner->profile_photo_path)
+									<img alt="profile picture" src="{{ url('/storage/' . $share->owner->profile_photo_path) }}" class="w-8 rounded-full mr-3">
+								@else
+									<img alt="generated profile picture" src="{{ url('https://ui-avatars.com/api/?name=' . $share->owner->name . '&background=random&length=1&size=128') }}" class="w-8 rounded-full mr-3">
+								@endif
+							</div>
+							<div class="text-2xl dark:text-gray-200">
+								{{ $share->owner->name }}
+							</div>
 						</div>
 					</button>
+					</div>
+					<div class="flex-grow w-full">
+						<livewire:toggle-privacy :share="$share">
+					</div>
+					</div>
 
 					<div class="grid grid-cols-1 md:grid-cols-3 gap-5" id="item-grid{{ $share-> id }}">
 						@foreach($share->items as $item)
 							@if ((!$item->claimed) || $item->claimant_id == auth()->user()->id)
-								<div id="newcard" class="grid grid-cols-1 justify-between bg-white overflow-hidden shadow-2xl rounded-2xl border dark:border-gray-500 dark:bg-gray-500 dark:text-gray-200">
+								<div id="{{ $item->id }}" class="user{{ $share->owner->id }} claimed{{$item->claimed}} grid grid-cols-1 justify-between bg-white overflow-hidden shadow-2xl rounded-2xl border dark:border-gray-500 dark:bg-gray-500 dark:text-gray-200">
 									<div class="w-full my-1 font-bold text-xl text-center mt-3">
 										@if ($item->url)
 											<a target="_blank" class="underline text-blue-600 dark:text-blue-900" href="{{$item->url}}">{{ $item->name }}</a>
