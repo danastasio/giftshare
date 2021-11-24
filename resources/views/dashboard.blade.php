03f7c4bb310b2cd76971aa6bdab37f697de657cd
David Anastasio <github@davidanastasio.com>
2021-11-24 10:10:39 -0500

    Prepare for text only card

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -60,9 +60,6 @@
 						@foreach($share->items as $item)
 							@if ((!$item->claimed) || $item->claimant_id == auth()->user()->id)
 								<div id="newcard" class="grid grid-cols-1 justify-between bg-white overflow-hidden shadow-2xl rounded-2xl border dark:border-gray-500 dark:bg-gray-500 dark:text-gray-200">
-									<div class="flex-none w-full mb-2 justify-center flex mx-auto mt-4">
-										<img src="{{ $item->image_url ?? url('/images/not_found.svg')}}" class="h-24" alt="product image">
-									</div>
 									<div class="w-full my-1 font-bold text-xl text-center mt-3">
 										@if ($item->url)
 											<a target="_blank" class="underline text-blue-600 dark:text-blue-900" href="{{$item->url}}">{{ $item->name }}</a>
@@ -70,10 +67,10 @@
 											{{ $item->name }}
 										@endif
 									</div>
-									<div class="w-full my-1 my-3 text-center text-gray-500 dark:text-gray-200">
+									<div class="w-full my-1 my-3 text-center text-gray-500 dark:text-gray-200 p-2">
 										<em>{{  $item->description ?? "No Description Provided" }}</em>
 									</div>
-									<div class="w-full h-full mt-auto">
+									<div class="mt-auto">
 										<livewire:claim-item :item="$item" class="w-full">
 									</div>
 								</div>
