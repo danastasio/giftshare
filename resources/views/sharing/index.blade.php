d84372128ff1c9d9f3c9263323aa7b0d235fec7a
David Anastasio <github@davidanastasio.com>
2022-05-08 10:04:59 -0400

    Fix spacing

--- resources/views/sharing/index.blade.php
+++ resources/views/sharing/index.blade.php
@@ -28,6 +28,7 @@
 							<div class="my-auto">
 								{{ $collection->name }}
 							</div>
+							<div class="flex-grow"></div>
 							<button class="rounded-full border p-2" type="button" onclick="document.getElementById('edit_collection_users_{{ $collection->id }}').classList.remove('invisible');">
 								<div class="flex">
 									<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
