9ef087fda07d1ec81e58cfa0bf6ae2fc78265a21
David Anastasio <david.d.anastasio@uconn.edu>
2022-05-10 15:04:13 -0400

    Add close button

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -48,26 +48,40 @@
 	@endphp
 @endif
 
-@if ( $errors->any() )
-	<div class="pt-5">
-		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-			<div class="text-white bg-red-700 rounded-lg p-4 text-center dark:text-gray-200 dark:bg-red-800">
-				<ul>
-					@foreach ($errors->all() as $error)
-						<li><strong>{{ __($error) }}</strong></li>
-					@endforeach
-				</ul>
+<div id="flash_messages">
+	@if ( $errors->any() )
+		<div class="flex">
+			<div class="pt-5">
+				<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+					<div class="text-white bg-red-700 rounded-lg p-4 text-center dark:text-gray-200 dark:bg-red-800">
+						<ul>
+							@foreach ($errors->all() as $error)
+								<li><strong>{{ __($error) }}</strong></li>
+							@endforeach
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div>
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+				</svg>
 			</div>
 		</div>
-	</div>
-@elseif (!empty($message))
-	<div class="mt-2">
-		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-			<div class="{{ $text_color }} {{ $bg_color }} rounded-lg p-4 text-center">
-				<strong>{{ $message }}</strong>
+	@elseif (!empty($message))
+		<div class="mt-2">
+			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+				<div class="relative {{ $text_color }} {{ $bg_color }} rounded-lg p-4 text-center">
+					<strong>{{ $message }}</strong>
+					<div class="absolute top-1 right-1">
+						<button onclick="document.getElementById('flash_messages').remove()">
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+							</svg>
+						</button>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-@else
-@endif
-
+	@endif
+</div>
