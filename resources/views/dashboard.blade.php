2dbcd01f1ab39d2a5fc243a7bf6b6db10b489660
David Anastasio <github@davidanastasio.com>
2022-05-20 09:39:48 -0400

    Remove control buttons

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -113,28 +113,6 @@
 												<div class="w-full my-1 my-3 text-center text-gray-500 dark:text-gray-200 p-2">
 													<em>{{  $item->description ?? "No Description Provided" }}</em>
 												</div>
-												<div class="mb-5 px-12">
-													<div class="grid grid-cols-3">
-														<button class="border rounded w-12 h-12 mx-auto bg-yellow-600">
-															<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-																<path stroke-linecap="round" stroke-linejoin="round" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
-															</svg>
-														</button>
-														<button class="border rounded w-12 h-12 mx-auto bg-orange-500">
-															<!-- Truck SVG -->
-															<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-																<path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
-																<path stroke-linecap="round" stroke-linejoin="round" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
-															</svg>
-														</button>
-														<button class="border rounded w-12 h-12 mx-auto bg-emerald-600">
-															<!-- Present SVG -->
-															<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-																<path stroke-linecap="round" stroke-linejoin="round" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-															</svg>
-														</button>
-													</div>
-												</div>
 												<div class="mt-auto">
 													<livewire:claim-item :item="$item" class="w-full">
 												</div>
